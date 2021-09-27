a134d96133722d99140635fd244ceb600b0e6da7
Boy-Chase <70419012+Boy-Chase@users.noreply.github.com>
2021-09-26 22:35:52 -0400

    so far so good, up to boost fixed, work on jump

--- Alternative Boost/Assets/Scripts/PlayerMovement.cs
+++ Alternative Boost/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    bool leftGo;
+    bool rightGo;
+    bool jumpGo;
+    bool boostGo;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //set all to false at start of game
+        leftGo = false;
+        rightGo = false;
+        jumpGo = false;
+        boostGo = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-          
+        //left movement
+        if (leftGo)
+        {
+            transform.Translate(1f, 0f, 0f);
+            leftGo = false;
+        }
+
+        //right movement
+        if (rightGo)
+        {
+            transform.Translate(-1f, 0, 0);
+            rightGo = false;
+        }
+
+        //jump movement
+        if (jumpGo)
+        {
+            GetComponent<Rigidbody>().AddForce(0f, 250f, 0f);
+            jumpGo = false;
+        }
+
+        //boost movement
+        if (boostGo)
+        {
+            StartCoroutine(boostWait());
+        }
+    }
+
+    //called on ButtonLeft
+    public void moveLeft()
+    {
+        leftGo = true;
+    }
+
+    //called on ButtonRight
+    public void moveRight()
+    {
+        rightGo = true;
+    }
+
+    //called on ButtonJump
+    public void jumpUp()
+    {
+        jumpGo = true;
+    }
+
+    //called on ButtonBoost
+    public void boostForward()
+    {
+        boostGo = true;
+    }
+
+    IEnumerator boostWait()
+    {
+        GetComponent<Rigidbody>().AddForce(0f, 0f, -0.1f, ForceMode.Impulse);
+
+        yield return new WaitForSeconds(1f);
+
+        GetComponent<Rigidbody>().AddForce(0f, 0f, 0.1f, ForceMode.Impulse);
+        boostGo = false;
     }
 }
