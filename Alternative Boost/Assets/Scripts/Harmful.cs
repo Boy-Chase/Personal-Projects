84c48e40878a296573d64799080fda97335814f0
Boy-Chase <70419012+Boy-Chase@users.noreply.github.com>
2021-11-02 14:22:57 -0400

    harm is set for testing

--- Alternative Boost/Assets/Scripts/Harmful.cs
+++ Alternative Boost/Assets/Scripts/Harmful.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Harmful : MonoBehaviour
 {
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,10 @@ public class Harmful : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // 3d distance formula
+        if( Mathf.Sqrt((Mathf.Pow((player.transform.position.x - transform.position.x), 2f) + Mathf.Pow((player.transform.position.y - transform.position.y), 2f) + Mathf.Pow((player.transform.position.z - transform.position.z), 2f))) < 1.0f)
+        {
+            player.GetComponentInChildren<Health>().dealDamage();
+        }
     }
 }
