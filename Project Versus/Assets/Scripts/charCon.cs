46f12dfb0da84bdf034c553e8a030a92166a126e
Boy-Chase <70419012+Boy-Chase@users.noreply.github.com>
2022-02-27 20:57:01 -0500

    basic input should be in

--- Project Versus/Assets/Scripts/charCon.cs
+++ Project Versus/Assets/Scripts/charCon.cs
@@ -9,11 +9,15 @@ public class charCon : MonoBehaviour
     // 0 = neutral
     // 1 = left dodge
     // 2 = right dodge
-    // 3 = attack (does nothing if meter too low)
+    // 3 = back step
+    // 4 = attack (does nothing if meter too low)
     public int manuever = 0;
 
     // amount of attack meter
     public int meter = 0;
+
+    // stores input as a 2D vector
+    private Vector2 playerInput;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +28,40 @@ public class charCon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // key a (left dodge)
+        if (playerInput.x < 0)
+        {
+            manuever = 1;
+        }
+
+        // key d (right dodge)
+        else if (0 < playerInput.x)
+        {
+            manuever = 2;
+        }
+
+        // key s (back step)
+        else if (playerInput.y < 0)
+        {
+            manuever = 3;
+        }
+
+        // key space (attack)
+        else if (0 < playerInput.y)
+        {
+            manuever = 4;
+        }
+
+        // otherwise nothing (neutral)
+        else
+        {
+            manuever = 0;
+        }
+    }
+
+    // updates the input vector
+    public void OnAction(InputValue value)
+    {
+        playerInput = value.Get<Vector2>();
     }
 }
