5e03e7677d8c324833268da3c8d54852313ac0d7
Boy-Chase <70419012+Boy-Chase@users.noreply.github.com>
2022-02-28 22:50:02 -0500

    input = cube position change

--- Project Versus/Assets/Scripts/charCon.cs
+++ Project Versus/Assets/Scripts/charCon.cs
@@ -13,6 +13,9 @@ public class charCon : MonoBehaviour
     // 4 = attack (does nothing if meter too low)
     public int manuever = 0;
 
+    // the previous input (used to check for change)
+    public int lastManuever = 0;
+
     // amount of attack meter
     public int meter = 0;
 
@@ -28,30 +31,34 @@ public class charCon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // INPUT
+        // INPUT TO ACTION:
 
         // key a (left dodge)
         if (playerInput.x < 0)
         {
             manuever = 1;
+            gameObject.transform.position = new Vector3(-0.35f, 0.0f, 0.0f);
         }
 
         // key d (right dodge)
         else if (0 < playerInput.x)
         {
             manuever = 2;
+            gameObject.transform.position = new Vector3(0.35f, 0.0f, 0.0f);
         }
 
         // key s (back step)
         else if (playerInput.y < 0)
         {
             manuever = 3;
+            gameObject.transform.position = new Vector3(0.0f, -0.1f , -0.3f);
         }
 
         // key space (attack)
         else if (0 < playerInput.y)
         {
             manuever = 4;
+            gameObject.transform.position = new Vector3(0.0f, 0.0f, 0.2f);
         }
 
         // otherwise nothing (neutral)
@@ -60,11 +67,8 @@ public class charCon : MonoBehaviour
             manuever = 0;
         }
 
-        // ACTION
-
-
-        // RESET
-        manuever = 0;
+        // set for next frame comparison
+        lastManuever = manuever;
     }
 
     
