fb059b855e11d6a1eccf7de1ef51c4bca9a7a9fd
Boy-Chase <70419012+Boy-Chase@users.noreply.github.com>
2022-03-01 22:37:30 -0500

    boom, good to make interactor

--- Project Versus/Assets/Scripts/enemyAI.cs
+++ Project Versus/Assets/Scripts/enemyAI.cs
@@ -23,6 +23,9 @@ public class enemyAI : MonoBehaviour
     // every 3 seconds
     public float threeSec = 0.0f;
 
+    // one sec commitment lock
+    public float oneSec = 0.0f;
+
     // odds of enemy moving
     public float actionChance;
 
@@ -38,8 +41,18 @@ public class enemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // reset stance
+        if (1.0f < oneSec)
+        {
+            manuever = 0;
+            gameObject.transform.position = new Vector3(0.0f, 1.0f, 1.0f);
+            oneSec = 0.0f;
+        }
+
         // add to timer if it hasn't been three seconds 
         threeSec += Time.deltaTime;
+
+        oneSec += Time.deltaTime;
 
         // maybe do something
         if (3.0f <= threeSec && !threeSecAttempt)
@@ -64,6 +77,7 @@ public class enemyAI : MonoBehaviour
                 {
                     manuever = 1;
                     gameObject.transform.position = new Vector3(-0.35f, 1.0f, 1.0f);
+                    oneSec = 0.0f;
                 }
 
                 // attack right
@@ -71,6 +85,7 @@ public class enemyAI : MonoBehaviour
                 {
                     manuever = 2;
                     gameObject.transform.position = new Vector3(0.35f, 1.0f, 1.0f);
+                    oneSec = 0.0f;
                 }
 
                 // attack sweep
@@ -78,6 +93,7 @@ public class enemyAI : MonoBehaviour
                 {
                     manuever = 3;
                     gameObject.transform.position = new Vector3(0.0f, 0.8f, 0.7f);
+                    oneSec = 0.0f;
                 }
             }
         }
@@ -96,6 +112,7 @@ public class enemyAI : MonoBehaviour
             {
                 manuever = 2;
                 gameObject.transform.position = new Vector3(-0.35f, 1.0f, 1.0f);
+                oneSec = 0.0f;
             }
 
             // attack right
@@ -103,6 +120,7 @@ public class enemyAI : MonoBehaviour
             {
                 manuever = 2;
                 gameObject.transform.position = new Vector3(0.35f, 1.0f, 1.0f);
+                oneSec = 0.0f;
             }
 
             // attack sweep
@@ -110,120 +128,11 @@ public class enemyAI : MonoBehaviour
             {
                 manuever = 2;
                 gameObject.transform.position = new Vector3(0.0f, 1.0f, 0.7f);
+                oneSec = 0.0f;
             }
-
-            // set for next frame comparison
-            lastManuever = manuever;
-        }
-
-        /*
-
-        // TIMER
-
-        // if timer is under a second
-        if (timer < 1.0f)
-        {
-            // nullify input bc still in commitment
-            playerInput = new Vector2(0.0f, 0.0f);
-
-            // add to timer
-            timer += Time.deltaTime;
-
-            // tried three sec attempt
-            threeSecAttempt = true;
-
-            // roll to maybe do something
-            actionChance = Random.Range(0.0f, 1.0f);
-
-            // do something
-            if (0.20f < actionChance)
-            {
-                // reset action cooldown
-                threeSec = 0.0f;
-
-                // we can try to do something in three seconds
-                threeSecAttempt = false;
-
-                // attack left
-                if (actionChance < 0.50f)
-                {
-                    manuever = 2;
-                    gameObject.transform.position = new Vector3(-0.35f, 1.0f, 1.0f);
-                }
-
-                // attack right
-                else if (0.51f < actionChance && actionChance < 0.80f)
-                {
-                    manuever = 2;
-                    gameObject.transform.position = new Vector3(0.35f, 1.0f, 1.0f);
-                }
-
-                // attack sweep
-                else
-                {
-                    manuever = 2;
-                    gameObject.transform.position = new Vector3(0.35f, 0.0f, 0.7f);
-                }
-            }
-
-            // should we ever get to five seconds, definetly do an action
-            if (5.0f < threeSec)
-            {
-                // reset action cooldown
-                threeSec = 0.0f;
-
-                // we can try to do something in three seconds
-                threeSecAttempt = false;
-
-            }
-
-            
-            // INPUT TO ACTION:
-
-            // key a (left dodge)
-            if (playerInput.x < 0)
-        {
-            manuever = 1;
-            gameObject.transform.position = new Vector3(-0.35f, 0.0f, 0.0f);
-        }
-
-        // key d (right dodge)
-        else if (0 < playerInput.x)
-        {
-            manuever = 2;
-            gameObject.transform.position = new Vector3(0.35f, 0.0f, 0.0f);
-        }
-
-        // key s (back step)
-        else if (playerInput.y < 0)
-        {
-            manuever = 3;
-            gameObject.transform.position = new Vector3(0.0f, -0.1f, -0.3f);
-        }
-
-        // key space (attack)
-        else if (0 < playerInput.y)
-        {
-            manuever = 4;
-            gameObject.transform.position = new Vector3(0.0f, 0.0f, 0.2f);
-        }
-
-        // if pose changes, start timer to begin commit 
-        if (manuever != lastManuever)
-        {
-            timer = 0.0f;
-        }
-
-        // no input (neutral)
-        if (1.0f <= timer)
-        {
-            manuever = 0;
-            gameObject.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
         }
 
         // set for next frame comparison
         lastManuever = manuever;
-
-            */
     }
 }
