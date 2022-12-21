143896cb3a3055ed98ce9101ed75fd281dd4ecdc
Boy-Chase <70419012+Boy-Chase@users.noreply.github.com>
2022-12-21 14:20:51 -0500

    added backround and stars

--- Demonic Space/Assets/Scripts/Player.cs
+++ Demonic Space/Assets/Scripts/Player.cs
@@ -24,12 +24,14 @@ public class Player : MonoBehaviour
     private List<Vector2> pastPositionsXY = new List<Vector2>();
 
     public float iFrames;
+    private float startLock;
 
     void Start()
     {
-        // set health + invincibilty
+        // set health + invincibilty + control lock
         health = 3;
         iFrames = 3.0f;
+        startLock = 3.0f;
 
         // default positions
         for (int x = 0; x < 100; x++)
@@ -45,6 +47,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         iFrames -= Time.deltaTime;
+        startLock -= Time.deltaTime;
 
         // all bullets in this script are made by the Player (not an enemy)
         bullet.GetComponent<Bullet>().playerMade = true;
@@ -52,36 +55,39 @@ public class Player : MonoBehaviour
         // move the player forward
         gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + 7.0f * Time.deltaTime);
 
-        // key a (move left)
-        if (playerInput.x < 0)
+        if (startLock < 0.0f)
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x - 4.0f * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
-        }
+            // key a (move left)
+            if (playerInput.x < 0)
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x - 4.0f * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
+            }
 
-        // key d (move right)
-        if (playerInput.x > 0)
-        {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x + 4.0f * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
-        }
+            // key d (move right)
+            if (playerInput.x > 0)
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x + 4.0f * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
+            }
 
-        // key w (move up)
-        if (playerInput.y > 0)
-        {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 4.0f * Time.deltaTime, gameObject.transform.position.z);
-        }
+            // key w (move up)
+            if (playerInput.y > 0)
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 4.0f * Time.deltaTime, gameObject.transform.position.z);
+            }
 
-        // key s (move down)
-        if (playerInput.y < 0)
-        {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 4.0f * Time.deltaTime, gameObject.transform.position.z);
-        }
+            // key s (move down)
+            if (playerInput.y < 0)
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 4.0f * Time.deltaTime, gameObject.transform.position.z);
+            }
 
-        // left click check
-        if (Input.GetMouseButtonDown(0))
-        { 
-            // make bullets
-            Instantiate(bullet, new Vector3(gameObject.transform.position.x - 0.75f, gameObject.transform.position.y, gameObject.transform.position.z + 0.5f), new Quaternion(gameObject.transform.rotation.x - 90, gameObject.transform.rotation.y, gameObject.transform.rotation.z, 1));
-            Instantiate(bullet, new Vector3(gameObject.transform.position.x + 0.75f, gameObject.transform.position.y, gameObject.transform.position.z + 0.5f), new Quaternion(gameObject.transform.rotation.x - 90, gameObject.transform.rotation.y, gameObject.transform.rotation.z, 1));
+            // left click check
+            if (Input.GetMouseButtonDown(0))
+            {
+                // make bullets
+                Instantiate(bullet, new Vector3(gameObject.transform.position.x - 0.75f, gameObject.transform.position.y, gameObject.transform.position.z + 0.5f), new Quaternion(gameObject.transform.rotation.x - 90, gameObject.transform.rotation.y, gameObject.transform.rotation.z, 1));
+                Instantiate(bullet, new Vector3(gameObject.transform.position.x + 0.75f, gameObject.transform.position.y, gameObject.transform.position.z + 0.5f), new Quaternion(gameObject.transform.rotation.x - 90, gameObject.transform.rotation.y, gameObject.transform.rotation.z, 1));
+            }
         }
 
         // save ship position
@@ -94,7 +100,7 @@ public class Player : MonoBehaviour
         // move camera
         myCamera.transform.position = new Vector3(pastPositionsXY[0].x, pastPositionsXY[0].y, position.z - 5.0f);
     }
-
+    
     // updates the input vector
     public void OnAction(InputValue value)
     {
