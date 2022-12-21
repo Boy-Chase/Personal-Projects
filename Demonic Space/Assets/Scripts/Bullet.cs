573b07850576a88be789f75daad8c6d95b55d63d
Boy-Chase <70419012+Boy-Chase@users.noreply.github.com>
2022-12-21 14:37:28 -0500

    lock + background, makes bullets spin + work on roll next

--- Demonic Space/Assets/Scripts/Bullet.cs
+++ Demonic Space/Assets/Scripts/Bullet.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     public bool playerMade = false;
+    public float rotate;
+    private float timer = 0.0f;
 
     // Update is called once per frame
     void Update()
     {
+        // after 10 sec, destroy bullet
+        timer += Time.deltaTime;
+        if(10.0f < timer)
+        {
+            Destroy(gameObject);
+        }
+
+        // rotate it
+        gameObject.transform.rotation = new Quaternion(gameObject.transform.rotation.x - 90, gameObject.transform.rotation.y, rotate + 200 * Time.deltaTime, 1);
+        rotate = gameObject.transform.rotation.z;
+
         if (playerMade)
         {
             // move the bullet forward
