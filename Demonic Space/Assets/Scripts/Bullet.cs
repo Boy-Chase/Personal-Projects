d3790dbbdfdbb5f120e398fa5a55c0d4e3cec854
Boy-Chase <70419012+Boy-Chase@users.noreply.github.com>
2022-12-23 15:40:02 -0500

    collider better rotates better

--- Demonic Space/Assets/Scripts/Bullet.cs
+++ Demonic Space/Assets/Scripts/Bullet.cs
@@ -20,18 +20,18 @@ public class Bullet : MonoBehaviour
         }
 
         // rotate it
-        gameObject.transform.rotation = new Quaternion(gameObject.transform.rotation.x - 90, gameObject.transform.rotation.y, rotate + 200 * Time.deltaTime, 1);
-        rotate = gameObject.transform.rotation.z;
+        gameObject.transform.Rotate(0, 0, gameObject.transform.rotation.z + 150f * Time.deltaTime);
+       // rotate = gameObject.transform.rotation.x;
 
         if (playerMade)
         {
             // move the bullet forward
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + 15.0f * Time.deltaTime);
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + 20.0f * Time.deltaTime);
         }
         else
         {
             // move the bullet backward
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z - 15.0f * Time.deltaTime);
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z - 20.0f * Time.deltaTime);
         }
     }
 }
