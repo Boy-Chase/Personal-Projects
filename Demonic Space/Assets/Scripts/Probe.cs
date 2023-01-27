08099f51aea42b58b1e3dc948b3eba0cef3a1137
Boy-Chase <70419012+Boy-Chase@users.noreply.github.com>
2023-01-27 14:18:37 -0500

    made attack probe

---
+++ Demonic Space/Assets/Scripts/Probe.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Probe : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Camera;
+
+    public float shootCooldown;
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // find our player + camera
+        Player = GameObject.FindGameObjectWithTag("player");
+        Camera = GameObject.FindGameObjectWithTag("MainCamera");
+
+        shootCooldown = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // increment cooldown
+        shootCooldown += Time.deltaTime;
+
+        // if cooldown is greater than sec, fire a bullet(s)
+        if (3.0f < shootCooldown)
+        {
+            Instantiate(bullet, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z - 1), new Quaternion(gameObject.transform.rotation.x - 90, gameObject.transform.rotation.y, gameObject.transform.rotation.z, 1));
+            bullet.GetComponent<Bullet>().playerMade = true;
+
+            shootCooldown = 0.0f;
+        }
+    }
+}
